8d2908e014137a2015c3d09163d074415ddae57c
evafeng <fh@falsehero.cn>
2020-12-06 21:11:00 +0800

    添加分支

--- src/main/java/cn/falsehero/demospringthymeleaf/DemoSpringThymeleafApplication.java
+++ src/main/java/cn/falsehero/demospringthymeleaf/DemoSpringThymeleafApplication.java
@@ -3,6 +3,11 @@ package cn.falsehero.demospringthymeleaf;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @Author: fenghao
+ * @Date: 2020/12/06 21:00
+ * @Description: 启动类
+ */
 @SpringBootApplication
 public class DemoSpringThymeleafApplication {
 
