4b14cbf9c0d25d49e6d2eb4b0d01e373d7680dbb
evafeng <fh@falsehero.cn>
2020-12-06 21:07:55 +0800

    创建项目

---
+++ src/test/java/cn/falsehero/demospringthymeleaf/DemoSpringThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.falsehero.demospringthymeleaf;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
